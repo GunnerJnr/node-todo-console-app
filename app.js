ee6ff65c860d688d93d6421a0e10397354dd2c67
David Gunner (Jnr) <gunnerjnr@live.co.uk>
2023-11-04 21:37:18 +0000

    Add the updateTodo function with comments explaining its functionality

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ function addTodo(key, value) {
   // or updates the value if the key already exists.
   todoList[key] = value;
 }
+
+/**
+* Updates the description of an existing TODO item.
+* @param {string} key - The unique identifier for the TODO item to update.
+* @param {string} newValue - The new description for the TODO item.
+*/
+function updateTodo(key, newValue) {
+  // Check if the TODO item exists in the list by its key.
+  if (todoList[key]) {
+      // Update the value associated with the key to the new value.
+      todoList[key] = newValue;
+  }
+}
